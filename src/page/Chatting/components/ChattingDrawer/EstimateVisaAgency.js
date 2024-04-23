1f702038cd387bcf7e89251b2f0d15ea7e3b6da0
whynot-byoul <whynot@hubilon.com>
2024-04-23 10:07:52 +0900

    1. 삭제하기 버튼 제거 2. 업로드 -> 작성하기로 변경 3. 일정 모달에 분류 dropdown box 추가

--- src/page/Chatting/components/ChattingDrawer/EstimateVisaAgency.js
+++ src/page/Chatting/components/ChattingDrawer/EstimateVisaAgency.js
@@ -1,169 +1,155 @@
-import React, { useState } from "react";
-import Button from "../../../../common/Button/Button";
-import Modal from "../../../../common/Modal/Modal";
-import FileInput from "../../../../common/FileInput/FileInput";
-import { EstimateVisaAgencyForm } from "./EstimateVisaAgencyForm";
-import ViewDocument from "./ViewDocument";
+import React, {useState} from 'react';
+import Button from '../../../../common/Button/Button';
+import Modal from '../../../../common/Modal/Modal';
+import FileInput from '../../../../common/FileInput/FileInput';
+import {EstimateVisaAgencyForm} from './EstimateVisaAgencyForm';
+import ViewDocument from './ViewDocument';
 
-const EstimateVisaAgency = ({ viewHandler }) => {
-  // 직접 추가한 서류 메뉴 여닫기
-  const [openMenu, setOpenMenu] = useState(false);
-  const openMenuHandler = () => {
-    setOpenMenu((current) => !current);
-  };
-  // 모달 여닫기
-  //openModal : 파일 업로드
-  const [openModal, setOpenModal] = useState(false);
-  const openModalHandler = () => {
-    if (openMenu) {
-      setOpenMenu(false);
-    }
-    setOpenModal((current) => !current);
-  };
-  // openModal2 : 비자발급 대행 견적서
-  const [openModal2, setOpenModal2] = useState(false);
-  const openModalHandler2 = () => {
-    setOpenModal2((current) => !current);
-  };
+const EstimateVisaAgency = ({viewHandler}) => {
+    // 직접 추가한 서류 메뉴 여닫기
+    const [openMenu, setOpenMenu] = useState(false);
+    const openMenuHandler = () => {
+        setOpenMenu((current) => !current);
+    };
+    // 모달 여닫기
+    //openModal : 파일 업로드
+    const [openModal, setOpenModal] = useState(false);
+    const openModalHandler = () => {
+        if (openMenu) {
+            setOpenMenu(false);
+        }
+        setOpenModal((current) => !current);
+    };
+    // openModal2 : 비자발급 대행 견적서
+    const [openModal2, setOpenModal2] = useState(false);
+    const openModalHandler2 = () => {
+        setOpenModal2((current) => !current);
+    };
 
-  // openModal3 : 비자발급 대행 견적서 보기
-  const [openModal3, setOpenModal3] = useState(false);
-  const openModalHandler3 = () => {
-    setOpenModal3((current) => !current);
-  };
+    // openModal3 : 비자발급 대행 견적서 보기
+    const [openModal3, setOpenModal3] = useState(false);
+    const openModalHandler3 = () => {
+        setOpenModal3((current) => !current);
+    };
 
-  // openModal4 : 비자발급 대행 보기(수정X)
-  const [openModal4, setOpenModal4] = useState(false);
-  const openModalHandler4 = () => {
-    setOpenModal4((current) => !current);
-  };
-  // input file 이벤트
-  const [fileName, setFileName] = useState("");
+    // openModal4 : 비자발급 대행 보기(수정X)
+    const [openModal4, setOpenModal4] = useState(false);
+    const openModalHandler4 = () => {
+        setOpenModal4((current) => !current);
+    };
+    // input file 이벤트
+    const [fileName, setFileName] = useState('');
 
-  const handleFileChange = (event) => {
-    const file = event.target.files[0];
-    if (file) {
-      setFileName(file.name);
-    }
-  };
-  return (
-    <>
-      <div className="chattingDrawerWrap">
-        <h3 className="title">
-          <span className="btn_prev" onClick={() => viewHandler(0)}>
-            이전
-          </span>
-          비자발급 대행 견적서
-        </h3>
-        <h4 className="subTitle">비자발급 대행 견적서</h4>
-        <div className="estimateWrap noFile">
-          <p className="fileName">견적서가 업로드 되지 않았습니다.</p>
-        </div>
+    const handleFileChange = (event) => {
+        const file = event.target.files[0];
+        if (file) {
+            setFileName(file.name);
+        }
+    };
+    return (
+        <>
+            <div className='chattingDrawerWrap'>
+                <h3 className='title'>
+                    <span className='btn_prev' onClick={() => viewHandler(0)}>
+                        이전
+                    </span>
+                    비자발급 대행 견적서
+                </h3>
+                <h4 className='subTitle'>비자발급 대행 견적서</h4>
+                <div className='estimateWrap noFile'>
+                    <p className='fileName'>견적서가 업로드 되지 않았습니다.</p>
+                </div>
 
-        <div className="estimateWrap">
-          <p className="fileName" onClick={openMenuHandler}>
-            비자발급 대행 견적서_20231215
-          </p>
-          <div className="dateTime">2023년 09월 30일 14:00</div>
-          <div className="btnHiddenMenu" onClick={openMenuHandler}></div>
-          <ul className={`hiddenMenu ${openMenu && "on"}`}>
-            <li onClick={openModalHandler}>수정하기</li>
-            <li onClick={openMenuHandler}>삭제하기</li>
-          </ul>
-        </div>
+                <div className='estimateWrap'>
+                    <p className='fileName' onClick={openMenuHandler}>
+                        비자발급 대행 견적서_20231215
+                    </p>
+                    <div className='dateTime'>2023년 09월 30일 14:00</div>
+                    <div className='btnHiddenMenu' onClick={openMenuHandler}></div>
+                    <ul className={`hiddenMenu ${openMenu && 'on'}`}>
+                        <li onClick={openModalHandler}>수정하기</li>
+                    </ul>
+                </div>
 
-        <div className="estimateWrap">
-          <p className="fileName" onClick={openModalHandler2}>
-            비자발급 대행 견적서_20231215
-          </p>
-          <div className="dateTime">2023년 09월 30일 14:00</div>
-        </div>
+                <div className='estimateWrap'>
+                    <p className='fileName' onClick={openModalHandler2}>
+                        비자발급 대행 견적서_20231215
+                    </p>
+                    <div className='dateTime'>2023년 09월 30일 14:00</div>
+                </div>
 
-        <div className="estimateWrap">
-          <p className="fileName" onClick={openModalHandler4}>
-            비자발급 대행 견적서_20231215비자발급 대행 견적서_20231215비자발급
-            대행 견적서_20231215
-          </p>
-          <div className="dateTime">2023년 09월 30일 14:00</div>
-          <span className="confirm">결제완료</span>
-        </div>
-        <div className="drawerBtnWrap">
-          <Button type="upload" fn={openModalHandler2}>
-            업로드
-          </Button>
-        </div>
-      </div>
-      {/* 파일 업로드 모달  */}
-      <Modal
-        className={openModal ? "open" : ""}
-        openModalHandler={openModalHandler}
-      >
-        <h3 className="modalTitle">파일 업로드</h3>
-        <div className="fileBox wide">
-          <h5 className="contentsTitle">
-            파일 첨부
-            <small>(jpg,jpeg,png,pdf만 첨부해 주세요.)</small>
-          </h5>
-          <FileInput handleFileChange={handleFileChange} fileName={fileName} />
-        </div>
-        <div className="modalBottom">
-          <Button type="secondary" size="lg" fn={openModalHandler}>
-            취소
-          </Button>
-          <Button type="primary" size="lg">
-            파일 업로드
-          </Button>
-        </div>
-      </Modal>
+                <div className='estimateWrap'>
+                    <p className='fileName' onClick={openModalHandler4}>
+                        비자발급 대행 견적서_20231215비자발급 대행 견적서_20231215비자발급 대행 견적서_20231215
+                    </p>
+                    <div className='dateTime'>2023년 09월 30일 14:00</div>
+                    <span className='confirm'>결제완료</span>
+                </div>
+                <div className='drawerBtnWrap'>
+                    <Button type='upload' fn={openModalHandler2}>
+                        작성하기
+                    </Button>
+                </div>
+            </div>
+            {/* 파일 업로드 모달  */}
+            <Modal className={openModal ? 'open' : ''} openModalHandler={openModalHandler}>
+                <h3 className='modalTitle'>파일 업로드</h3>
+                <div className='fileBox wide'>
+                    <h5 className='contentsTitle'>
+                        파일 첨부
+                        <small>(jpg,jpeg,png,pdf만 첨부해 주세요.)</small>
+                    </h5>
+                    <FileInput handleFileChange={handleFileChange} fileName={fileName} />
+                </div>
+                <div className='modalBottom'>
+                    <Button type='secondary' size='lg' fn={openModalHandler}>
+                        취소
+                    </Button>
+                    <Button type='primary' size='lg'>
+                        파일 업로드
+                    </Button>
+                </div>
+            </Modal>
 
-      {/* 비자발급 대행 모달 */}
-      <Modal
-        className={openModal2 ? "open" : ""}
-        openModalHandler={openModalHandler2}
-      >
-        <h3 className="modalTitle">비자발급 대행 견적서</h3>
-        <div class="modalScrollBox">
-          <EstimateVisaAgencyForm />
-          <div className="modalBottom">
-            <Button type="secondary" size="lg" fn={openModalHandler2}>
-              취소
-            </Button>
-            <Button type="primary" size="lg">
-              저장 및 공유
-            </Button>
-          </div>
-        </div>
-      </Modal>
+            {/* 비자발급 대행 모달 */}
+            <Modal className={openModal2 ? 'open' : ''} openModalHandler={openModalHandler2}>
+                <h3 className='modalTitle'>비자발급 대행 견적서</h3>
+                <div class='modalScrollBox'>
+                    <EstimateVisaAgencyForm />
+                    <div className='modalBottom'>
+                        <Button type='secondary' size='lg' fn={openModalHandler2}>
+                            취소
+                        </Button>
+                        <Button type='primary' size='lg'>
+                            저장 및 공유
+                        </Button>
+                    </div>
+                </div>
+            </Modal>
 
-      {/* 비자발급 대행 모달 */}
-      <Modal
-        className={openModal3 ? "open" : ""}
-        openModalHandler={openModalHandler3}
-      >
-        <h3 className="modalTitle">비자발급 대행 견적서</h3>
-        {/* ↓ exceptBtn : 스크롤 영역에 버튼을 제외시키는 경우 */}
-        <div className="modalScrollBox exceptBtn">
-          <ViewDocument />
-        </div>
-        <div className="modalBottom">
-          <Button type="primary" size="lg" fn={openModalHandler3}>
-            수정
-          </Button>
-        </div>
-      </Modal>
-      {/* 비자발급 대행 모달 : 결제 완료*/}
-      <Modal
-        className={openModal4 ? "open" : ""}
-        openModalHandler={openModalHandler4}
-      >
-        <h3 className="modalTitle">비자발급 대행 견적서</h3>
-        {/* ↓ exceptBtn : 스크롤 영역에 버튼을 제외시키는 경우 */}
-        <div className="modalScrollBox exceptBtn">
-          <ViewDocument />
-        </div>
-      </Modal>
-    </>
-  );
+            {/* 비자발급 대행 모달 */}
+            <Modal className={openModal3 ? 'open' : ''} openModalHandler={openModalHandler3}>
+                <h3 className='modalTitle'>비자발급 대행 견적서</h3>
+                {/* ↓ exceptBtn : 스크롤 영역에 버튼을 제외시키는 경우 */}
+                <div className='modalScrollBox exceptBtn'>
+                    <ViewDocument />
+                </div>
+                <div className='modalBottom'>
+                    <Button type='primary' size='lg' fn={openModalHandler3}>
+                        수정
+                    </Button>
+                </div>
+            </Modal>
+            {/* 비자발급 대행 모달 : 결제 완료*/}
+            <Modal className={openModal4 ? 'open' : ''} openModalHandler={openModalHandler4}>
+                <h3 className='modalTitle'>비자발급 대행 견적서</h3>
+                {/* ↓ exceptBtn : 스크롤 영역에 버튼을 제외시키는 경우 */}
+                <div className='modalScrollBox exceptBtn'>
+                    <ViewDocument />
+                </div>
+            </Modal>
+        </>
+    );
 };
 
 export default EstimateVisaAgency;
--- src/page/Chatting/components/ScheduleBox/ScheduleBox.js
+++ src/page/Chatting/components/ScheduleBox/ScheduleBox.js
@@ -12,107 +12,115 @@ import Button from '../../../../common/Button/Button';
 import ScheduleDetail from './ScheduleDetail';
 
 const ScheduleBox = ({close}) => {
-  const [coordX, setCoordX] = useState(0);
-  const [coordY, setCoordY] = useState(0);
-  const handleClick = (event) => {
-    const x = event.clientX;
-    const y = event.clientY;
-    setCoordX(x);
-    setCoordY(y);
-    // console.log(`Clicked at coordinates: (${x}, ${y})`);
-  };
-  const [modalOpen, setModalOpen] = useState(false);
-  const modalCloseHandler = () => {
-    setModalOpen(false);
-  };
-  const [modalOpen2, setModalOpen2] = useState(false);
-  const modalCloseHandler2 = () => {
-    setModalOpen2(false);
-  };
+    const [coordX, setCoordX] = useState(0);
+    const [coordY, setCoordY] = useState(0);
+    const handleClick = (event) => {
+        const x = event.clientX;
+        const y = event.clientY;
+        setCoordX(x);
+        setCoordY(y);
+        // console.log(`Clicked at coordinates: (${x}, ${y})`);
+    };
+    const [modalOpen, setModalOpen] = useState(false);
+    const modalCloseHandler = () => {
+        setModalOpen(false);
+    };
+    const [modalOpen2, setModalOpen2] = useState(false);
+    const modalCloseHandler2 = () => {
+        setModalOpen2(false);
+    };
 
-  const handleDateClick = (arg) => {
-    if (modalOpen2) {
-      setModalOpen2(false);
-    }
+    const handleDateClick = (arg) => {
+        if (modalOpen2) {
+            setModalOpen2(false);
+        }
+        console.log(coordY);
+        // if (coordY > 300 && coordY < 400) {
+        //     setCoordY(coordY - 160);
+        // } else if (coordY > 400 && coordY < 560) {
+        //     setCoordY(coordY - 310);
+        // } else if (coordY > 560) {
+        //     setCoordY(150);
+        // }
+        if (coordX > 580) {
+            setCoordX(coordX - 550);
+        }
+        setModalOpen(true);
+    };
 
-    if (coordY > 300 && coordY < 400) {
-      setCoordY(coordY - 130);
-    } else if (coordY > 400 && coordY < 560) {
-      setCoordY(coordY - 280);
-    } else if (coordY > 560) {
-      setCoordY(150);
-    }
-    if (coordX > 580) {
-      setCoordX(coordX - 550);
-    }
-    setModalOpen(true);
-  };
-
-  const handleEventClick = (eventInfo) => {
-    if (modalOpen) {
-      setModalOpen(false);
-    }
-    if (coordY > 300 && coordY < 400) {
-      setCoordY(coordY - 130);
-    } else if (coordY > 400 && coordY < 560) {
-      setCoordY(coordY - 280);
-    } else if (coordY > 560) {
-      setCoordY(150);
-    }
-    if (coordX > 580) {
-      setCoordX(coordX - 550);
-    }
-    setModalOpen2(true);
-  };
-  const events = [{title: 'Meeting', start: '2024-04-20'}];
-  function renderEventContent(eventInfo) {
-    return (
-      <div className='evnetBar'>
-        <b>{eventInfo.timeText}</b>
-        <i>{eventInfo.event.title}</i>
-      </div>
-    );
-  }
-  return (
-    <div className='scheduleBoxWrap' onClick={handleClick}>
-      <h2 className='title'>
-        일정
-        <button className='close' onClick={close}>
-          close
-        </button>
-      </h2>
-
-      <div className='calendarWrap'>
-        <FullCalendar
-          locales={[koLocale]}
-          initialView='dayGridMonth'
-          locale='ko'
-          plugins={[dayGridPlugin, interactionPlugin]}
-          dateClick={handleDateClick}
-          events={events}
-          eventContent={renderEventContent}
-          eventClick={handleEventClick}
-        />
-      </div>
-      <div className='customPositionModal' style={{top: coordY, left: coordX}}>
-        <Modal className={modalOpen ? 'open' : ''} openModalHandler={modalCloseHandler} size='md' type='noneDim'>
-          <div className='modalScrollBox'>
-            <ScheduleForm />
-            <div className='btnRight'>
-              <Button type='primary' fn={modalCloseHandler}>
-                저장
-              </Button>
+    const handleEventClick = (eventInfo) => {
+        if (modalOpen) {
+            setModalOpen(false);
+        }
+        if (coordY > 300 && coordY < 400) {
+            setCoordY(coordY - 130);
+        } else if (coordY > 400 && coordY < 560) {
+            setCoordY(coordY - 280);
+        } else if (coordY > 560) {
+            setCoordY(150);
+        }
+        if (coordX > 580) {
+            setCoordX(coordX - 550);
+        }
+        setModalOpen2(true);
+    };
+    const events = [{title: 'Meeting', start: '2024-04-20'}];
+    function renderEventContent(eventInfo) {
+        return (
+            <div className='evnetBar'>
+                <b>{eventInfo.timeText}</b>
+                <i>{eventInfo.event.title}</i>
             </div>
-          </div>
-        </Modal>
-      </div>
+        );
+    }
+    return (
+        <div className='scheduleBoxWrap'>
+            <h2 className='title'>
+                일정
+                <button className='close' onClick={close}>
+                    close
+                </button>
+            </h2>
 
-      <div className='customPositionModal' style={{top: coordY, left: coordX}}>
-        <Modal className={modalOpen2 ? 'open' : ''} openModalHandler={modalCloseHandler2} size='md' type='noneDim'>
-          <ScheduleDetail />
-        </Modal>
-      </div>
-    </div>
-  );
+            <div className='calendarWrap' onClick={handleClick}>
+                <FullCalendar
+                    locales={[koLocale]}
+                    initialView='dayGridMonth'
+                    locale='ko'
+                    plugins={[dayGridPlugin, interactionPlugin]}
+                    dateClick={handleDateClick}
+                    events={events}
+                    eventContent={renderEventContent}
+                    eventClick={handleEventClick}
+                />
+            </div>
+            <div className='customPositionModal' style={{top: '90px', left: coordX}}>
+                <Modal
+                    className={modalOpen ? 'open' : ''}
+                    openModalHandler={modalCloseHandler}
+                    size='md'
+                    type='noneDim'
+                >
+                    <ScheduleForm />
+                    <div className='btnRight'>
+                        <Button type='primary' fn={modalCloseHandler}>
+                            저장
+                        </Button>
+                    </div>
+                </Modal>
+            </div>
+
+            <div className='customPositionModal' style={{top: coordY, left: coordX}}>
+                <Modal
+                    className={modalOpen2 ? 'open' : ''}
+                    openModalHandler={modalCloseHandler2}
+                    size='md'
+                    type='noneDim'
+                >
+                    <ScheduleDetail />
+                </Modal>
+            </div>
+        </div>
+    );
 };
 export default ScheduleBox;
